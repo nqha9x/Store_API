2605b3a20fca72a2e2fd7598b10ae9dc3d8c6f1c
Ha Nguyen <nqha9x@gmail.com>
2021-12-23 13:07:40 +0700

    new commit for REGISTER and LOGIN

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user.js')
+const authRoute = require('./routes/auth.js')
+
+const app = express()
+
+// connect mongoose
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('DB Connection successfull!'))
+.catch((err) => {
+    console.log(err)
+})
+
+// read properties
+app.use(express.json())
+
+// routing
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+
+app.get('/', (req, res) => {
+    res.send('[HOME route]')
+})
+
+// server listening
+app.listen(process.env.PORT || 1999, () => {
+    console.log(`SERVER listening on PORT: http://localhost:1999`)
+})
+
+
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User.js')
+const CryptoJS = require('crypto-js')
+
+// REGISTER
+router.post('/register', async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.Rabbit.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    })
+
+    try {
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        !user && res.status(401).json('Wrong information')
+
+        const hashedPassword = CryptoJS.Rabbit.decrypt(user.password, process.env.PASS_SEC)
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8)        
+
+        password !== req.body.password && res.status(401).json('Wrong information')        
+
+        res.status(200).json(user)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
