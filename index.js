d64a9c9645bdac025aff52abfbabb8737f4ff3a3
Ha Nguyen <nqha9x@gmail.com>
2021-12-23 18:09:39 +0700

    PRODUCT with CRUD OPERATIONS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user.js')
 const authRoute = require('./routes/auth.js')
+const productRoute = require('./routes/product.js')
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.use(express.json())
 // routing
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
 
 app.get('/', (req, res) => {
     res.send('[HOME route]')
