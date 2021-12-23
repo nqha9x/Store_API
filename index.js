ff87abbf63851aa48f58e3929b6687bb23ca7424
Ha Nguyen <nqha9x@gmail.com>
2021-12-24 02:26:15 +0700

    CART with CRUD OPERATIONS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const userRoute = require('./routes/user.js')
 const authRoute = require('./routes/auth.js')
 const productRoute = require('./routes/product.js')
 const orderRoute = require('./routes/order.js')
+const cartRoute = require('./routes/cart.js')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/orders', orderRoute)
+app.use('/api/carts', cartRoute)
 
 app.get('/', (req, res) => {
     res.send('[HOME route]')
--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,67 @@
 const express = require('express')
 const router = express.Router()
+const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require('./verifyToken.js')
+const Cart = require('../models/Product.js')
 
+// CREATE CART
+router.post('/', verifyToken, async (req, res) => {
+    const newCart = new Cart(req.body);
+                                         
+    try {
+        const savedCart = await newCart.save() 
+        res.status(200).json(savedCart);
+    } 
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
 
+// UPDATE CART
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {    
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(
+            req.params.id, 
+            {
+                $set: req.body
+            },
+            { new: true }
+        )
+        res.status(200).json(updatedCart)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE CART
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res)=>{
+    try {
+      await Cart.findByIdAndDelete(req.params.id)
+      res.status(200).json("Cart has been deleted!")
+    }catch(err) {
+      res.status(500).json(err)
+    }
+})
+
+// GET CART by id
+router.get('/:userId', verifyTokenAndAuthorization, async (req, res)=>{
+    try {
+        const cart =  await Cart.findOne({userId: req.params.userId})
+        res.status(200).json(cart)
+    }
+    catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET ALL CART
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+       const carts = await Cart.find();
+       res.status(200).json(carts)
+      } catch (err) {
+       res.status(500).json(err)
+     }
+})
 
 module.exports = router
